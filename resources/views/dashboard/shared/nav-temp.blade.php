388b423c1f36fd2ee99fbad3b5bb1c32b6a7095a
Bestsolution <coolmind2028@outlook.com>
2020-08-12 16:41:29 +0800

    first commit

---
+++ resources/views/dashboard/shared/nav-temp.blade.php
@@ -0,0 +1,38 @@
+      <div class="c-sidebar-brand"><img class="c-sidebar-brand-full" src="/assets/brand/coreui-base-white.svg"
+          width="118" height="46" alt="CoreUI Logo"><img class="c-sidebar-brand-minimized"
+          src="assets/brand/coreui-signet-white.svg" width="118" height="46" alt="CoreUI Logo"></div>
+      <nav class="c-sidebar-nav">
+        <ul class="c-sidebar-nav">
+          <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link" href="/">
+              <svg class="c-sidebar-nav-icon">
+                <use xlink:href="/assets/icons/coreui/free-symbol-defs.svg#cui-speedometer"></use>
+              </svg> Analytics </a></li>
+
+          <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link" href="/">
+              <svg class="c-sidebar-nav-icon">
+                <use xlink:href="/assets/icons/coreui/free-symbol-defs.svg#cui-speedometer"></use>
+              </svg> Content </a></li>
+
+          <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link" href="/">
+              <svg class="c-sidebar-nav-icon">
+                <use xlink:href="/assets/icons/coreui/free-symbol-defs.svg#cui-speedometer"></use>
+              </svg> Administration </a></li>
+
+          <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link" href="/login" target="_top">
+              <svg class="c-sidebar-nav-icon">
+                <use xlink:href="/assets/icons/coreui/free-symbol-defs.svg#cui-account-logout"></use>
+              </svg> Login</a></li>
+          <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link" href="/register" target="_top">
+              <svg class="c-sidebar-nav-icon">
+                <use xlink:href="/assets/icons/coreui/free-symbol-defs.svg#cui-account-logout"></use>
+              </svg> Register</a></li>
+        </ul>
+      </nav>
+      <button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent"
+        data-class="c-sidebar-minimized"></button>
+      </div>
